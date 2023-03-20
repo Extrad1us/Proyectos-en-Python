2b3b118b12517700603ca8535bc1802c6a2323a4
Extradius <128345443+Extrad1us@users.noreply.github.com>
2023-03-20 11:36:16 -0500

    Agrege unas correciones en Como_Preparar_Una_Bebida

--- Como_Preparar_Una_Bebida.py
+++ Como_Preparar_Una_Bebida.py
@@ -1,13 +1,13 @@
-print("***COMO PREPARAR UN COLACAO***")
+print("***COMO PREPARAR UN MILO***")
 
 print("Nos dirigimos a la cocina")
 
 print("*revisamos la despensa*")
-print("Buscamos el colacao")
-hay_colaco = (input("¿Habria colacao en la alacena? (Si/No)"))
-if hay_colaco == "Si":
+print("Buscamos el Milo")
+hay_Milo = (input("¿Habria Milo en la alacena? (Si/No)"))
+if hay_Milo == "Si":
     print("sacamos el colacao")
-if hay_colaco == "No":
+if hay_Milo == "No":
     print("Que no, tienes compra")
 
 print("*buscamos la leche*")
@@ -18,28 +18,28 @@ if hay_leche == "Si":
 if hay_leche == "No":
     print("No hay leche pringao")
 
-if hay_leche == "No" or hay_colaco == "No":
+if hay_leche == "No" or hay_Milo == "No":
     print("vales pura verga, ve a comprar al super..")
 
-if hay_colaco == "No":
-    print("Compras colacao")
+if hay_Milo == "No":
+    print("Compras Milo")
 
 if hay_leche == "No":
     print("Compras leche")
 
-if hay_leche and hay_colaco == "Si":
+if hay_leche and hay_Milo == "Si":
     print("Busquemos un vaso para preparalo")
 
 print("Ahora si viene lo chido")
 
-vaso_colaco = input("Ya tienes el vaso listo? (Si/No)")
+vaso_Milo = input("Ya tienes el vaso listo? (Si/No)")
 
-if vaso_colaco == "Si":
-    print("Agrega el colacao al vaso")
+if vaso_Milo == "Si":
+    print("Agrega el Milo al vaso")
     print("despues agrega la leche y ve buscando una cuchara")
-    print("Ya con la cuchara en la mano, revuelve tu colacao hasta que se integre bien a la leche")
+    print("Ya con la cuchara en la mano, revuelve tu Milo hasta que se integre bien a la leche")
     print("ya todo integrado y rico disfruta")
 
-if vaso_colaco != "Si":
+if vaso_Milo != "Si":
     print("Espabila como no vas a tener un vaso en casa?")
     print("Largate antes que te pele")
