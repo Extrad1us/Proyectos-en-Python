e36ec6345b8e7a4144c1b13e12028b1278de0874
Extradius <128345443+Extrad1us@users.noreply.github.com>
2023-03-21 09:07:25 -0500

    Archivo desarrollado en clases

---
+++ Clase.py
@@ -0,0 +1,5 @@
+nombre = input("Ingresa tu nombre")
+edad = int (input("Ingresa tu edad"))
+estatura = float(input("Ingresa tu estatura"))
+
+print ( "hola", nombre, "tienes", edad, "años, y mides", estatura, "metros")
