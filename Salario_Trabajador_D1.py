031469e2b11a5f05dc975426ebfdaf3696db4b06
Extradius <128345443+Extrad1us@users.noreply.github.com>
2023-03-21 20:30:04 -0500

    Plantilla del ejercicio calificable clase de fundamentos

---
+++ Salario_Trabajador_D1.py
@@ -0,0 +1,32 @@
+'''
+Fundamentos de Programación Imperativa
+Nombre y código del autor: Luis Angel Arango Marin (2360181-2724)
+Fecha de realización: 21/03/2023
+
+'''
+#==============================================
+#            FUNCIÓNES AUXILIARES
+#==============================================
+'''
+Por cada función auxiliar y según el diseño realizado transcriba.
+'''
+# Proposito:
+# Contrato:
+# Ejemplo:
+# Encabezado
+# Cuerpo
+
+
+#==============================================
+#           FUNCIÓN PRINCIPAL
+#==============================================
+# Proposito:
+# Contrato:
+# Ejemplo:
+# Encabezado
+# Cuerpo
+            
+
+#==============================================
+#           LLAMADO A LA FUNCIÓN PRINCIPAL
+#==============================================
