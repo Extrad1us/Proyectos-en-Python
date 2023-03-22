a143c071281b17c9646a9c3278551f89ccdfd787
Extradius <128345443+Extrad1us@users.noreply.github.com>
2023-03-21 20:26:21 -0500

    Modificado el archivo de clase

--- Clase.py
+++ Clase.py
@@ -2,4 +2,8 @@ nombre = input("Ingresa tu nombre")
 edad = int (input("Ingresa tu edad"))
 estatura = float(input("Ingresa tu estatura"))
 
-print ( "hola", nombre, "tienes", edad, "años, y mides", estatura, "metros")
+#Las comas importan en este tipo de conjunciones de texto y definiciones
+print ( "hola", nombre, "tienes", edad, "años, y mides", estatura, "metros") 
+
+if edad < 18 :
+    print("eres mayor de edad")
